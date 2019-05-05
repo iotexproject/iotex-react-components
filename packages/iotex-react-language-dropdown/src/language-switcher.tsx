2e73a546db7ec223e7c42f0d7e9ef7ba4d5db6aa
Solan <anshuomomo@gmail.com>
2019-05-05 18:28:26 +0800

    some improve for used

--- packages/iotex-react-language-dropdown/src/language-switcher.tsx
+++ packages/iotex-react-language-dropdown/src/language-switcher.tsx
@@ -1,5 +1,6 @@
 // tslint:disable:no-var-requires
 // tslint:disable:no-http-string
+import { Icon } from "antd";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { colors } from "./style-color";
@@ -13,10 +14,24 @@ const MEDIA_DROPDOWN_MENU = `@media only screen and (maxWidth: 900px)`;
 const GOTO_TRANS = "GOTO_TRANS";
 const GOOGLE_TRANSLATE = "GOOGLE_TRANSLATE";
 
-const languages = [
+const languagesMap: Array<languageMenu> = [
   { value: Languages.EN, children: "English" },
-  { value: Languages.ZH_CN, children: "简体中文" }
+  { value: Languages.KO, children: "한국어" },
+  { value: Languages.ZH_CN, children: "简体中文" },
+  { value: Languages.RU, children: "Русский" },
+  { value: Languages.IT, children: "Italiano" }
 ];
+
+const googleToolMap: Array<languageMenu> = [
+  { value: GOTO_TRANS, children: "Help Translation?" },
+  { value: GOOGLE_TRANSLATE, children: "Google Translation" }
+];
+
+// tslint:disable:no-any
+type languageMenu = {
+  value: any;
+  children: string;
+};
 
 interface State {
   displayTranslationMenu: boolean;
@@ -25,6 +40,8 @@ interface State {
 
 interface Props {
   style?: object;
+  supportLanguages?: [];
+  googleTools?: boolean;
 }
 
 class LanguageSwitcher extends Component<Props, State> {
@@ -56,6 +73,27 @@ class LanguageSwitcher extends Component<Props, State> {
     return undefined;
   };
 
+  public initLanguageMenu(props: Props): Array<languageMenu> {
+    const { supportLanguages, googleTools } = props;
+
+    const userLanguageMenus: Array<languageMenu> = languagesMap.filter(o => {
+      return (
+        supportLanguages &&
+        supportLanguages.some(j => {
+          return o.value === j;
+        })
+      );
+    });
+
+    if (userLanguageMenus.length > 0) {
+      return googleTools
+        ? userLanguageMenus.concat(googleToolMap)
+        : userLanguageMenus;
+    }
+
+    return googleTools ? languagesMap.concat(googleToolMap) : languagesMap;
+  }
+
   public render(): JSX.Element {
     let uri = "";
     if (window.location) {
@@ -70,7 +108,12 @@ class LanguageSwitcher extends Component<Props, State> {
       return "white";
     };
 
-    const { style = { width: "1em" }, ...others } = this.props;
+    const {
+      style = { width: "3em", display: "inline-block" },
+      ...others
+    } = this.props;
+    const languages = this.initLanguageMenu(this.props);
+
     const translationBlock = (
       <div
         style={{
@@ -250,7 +293,7 @@ const LanguageSwitchButton = ({ children, ...props }) => {
     <button
       {...props}
       style={{
-        padding: "7px 0px 0px 0px",
+        padding: "4px 0 0",
         backgroundColor: "transparent",
         borderStyle: "none",
         cursor: "pointer",
@@ -278,27 +321,13 @@ const LanguageItem = ({ children }) => {
 };
 
 // @ts-ignore
-const TranslationIcon = ({ style }) => {
-  return (
-    <svg
-      style={style}
-      aria-hidden="true"
-      focusable="false"
-      data-prefix="fas"
-      data-icon="language"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 640 512"
-      className="svg-inline--fa fa-language fa-w-20 fa-3x"
-    >
-      <path
-        fill="currentColor"
-        d="M152.1 236.2c-3.5-12.1-7.8-33.2-7.8-33.2h-.5s-4.3 21.1-7.8 33.2l-11.1 37.5H163zM616 96H336v320h280c13.3 0 24-10.7 24-24V120c0-13.3-10.7-24-24-24zm-24 120c0 6.6-5.4 12-12 12h-11.4c-6.9 23.6-21.7 47.4-42.7 69.9 8.4 6.4 17.1 12.5 26.1 18 5.5 3.4 7.3 10.5 4.1 16.2l-7.9 13.9c-3.4 5.9-10.9 7.8-16.7 4.3-12.6-7.8-24.5-16.1-35.4-24.9-10.9 8.7-22.7 17.1-35.4 24.9-5.8 3.5-13.3 1.6-16.7-4.3l-7.9-13.9c-3.2-5.6-1.4-12.8 4.2-16.2 9.3-5.7 18-11.7 26.1-18-7.9-8.4-14.9-17-21-25.7-4-5.7-2.2-13.6 3.7-17.1l6.5-3.9 7.3-4.3c5.4-3.2 12.4-1.7 16 3.4 5 7 10.8 14 17.4 20.9 13.5-14.2 23.8-28.9 30-43.2H412c-6.6 0-12-5.4-12-12v-16c0-6.6 5.4-12 12-12h64v-16c0-6.6 5.4-12 12-12h16c6.6 0 12 5.4 12 12v16h64c6.6 0 12 5.4 12 12zM0 120v272c0 13.3 10.7 24 24 24h280V96H24c-13.3 0-24 10.7-24 24zm58.9 216.1L116.4 167c1.7-4.9 6.2-8.1 11.4-8.1h32.5c5.1 0 9.7 3.3 11.4 8.1l57.5 169.1c2.6 7.8-3.1 15.9-11.4 15.9h-22.9a12 12 0 0 1-11.5-8.6l-9.4-31.9h-60.2l-9.1 31.8c-1.5 5.1-6.2 8.7-11.5 8.7H70.3c-8.2 0-14-8.1-11.4-15.9z"
-        className=""
-      />
-    </svg>
-  );
-};
+const TranslationIcon = props => <Icon component={TranslationSvg} {...props} />;
+
+const TranslationSvg = () => (
+  <svg fill="currentColor" viewBox="0 0 1024 500">
+    <path d="M152.1 236.2c-3.5-12.1-7.8-33.2-7.8-33.2h-.5s-4.3 21.1-7.8 33.2l-11.1 37.5H163zM616 96H336v320h280c13.3 0 24-10.7 24-24V120c0-13.3-10.7-24-24-24zm-24 120c0 6.6-5.4 12-12 12h-11.4c-6.9 23.6-21.7 47.4-42.7 69.9 8.4 6.4 17.1 12.5 26.1 18 5.5 3.4 7.3 10.5 4.1 16.2l-7.9 13.9c-3.4 5.9-10.9 7.8-16.7 4.3-12.6-7.8-24.5-16.1-35.4-24.9-10.9 8.7-22.7 17.1-35.4 24.9-5.8 3.5-13.3 1.6-16.7-4.3l-7.9-13.9c-3.2-5.6-1.4-12.8 4.2-16.2 9.3-5.7 18-11.7 26.1-18-7.9-8.4-14.9-17-21-25.7-4-5.7-2.2-13.6 3.7-17.1l6.5-3.9 7.3-4.3c5.4-3.2 12.4-1.7 16 3.4 5 7 10.8 14 17.4 20.9 13.5-14.2 23.8-28.9 30-43.2H412c-6.6 0-12-5.4-12-12v-16c0-6.6 5.4-12 12-12h64v-16c0-6.6 5.4-12 12-12h16c6.6 0 12 5.4 12 12v16h64c6.6 0 12 5.4 12 12zM0 120v272c0 13.3 10.7 24 24 24h280V96H24c-13.3 0-24 10.7-24 24zm58.9 216.1L116.4 167c1.7-4.9 6.2-8.1 11.4-8.1h32.5c5.1 0 9.7 3.3 11.4 8.1l57.5 169.1c2.6 7.8-3.1 15.9-11.4 15.9h-22.9a12 12 0 0 1-11.5-8.6l-9.4-31.9h-60.2l-9.1 31.8c-1.5 5.1-6.2 8.7-11.5 8.7H70.3c-8.2 0-14-8.1-11.4-15.9z" />
+  </svg>
+);
 
 // todo: move to a common file?
 function updateQueryStringParameter(
@@ -325,3 +354,5 @@ export const LanguageSwitcherContainer = connect(
     return {};
   }
 )(LanguageSwitcher);
+
+export const SupportLanguages = Languages;
