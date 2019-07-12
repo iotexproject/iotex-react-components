c8c1c26b34ec5e965ef6cacc84c14b9b8206b3cd
sxlwar <sxlwar@163.com>
2019-07-12 15:32:04 +0800

    fix(#155): add ava but no test case

--- packages/iotex-i18n-monitor/src/__test__/index.test.ts
+++ packages/iotex-i18n-monitor/src/__test__/index.test.ts
@@ -1,11 +1,6 @@
 import test from "ava";
-// import { i18nMonitor } from "../index";
 
+// TODO: add test case;
 test("i18n monitor run", t => {
-  // i18nMonitor({
-  //   source: "../translations/",
-  //   monitorLanguages: ["en", "zh-CN"],
-  //   output: "../translations/update/"
-  // });
   t.pass();
 });
