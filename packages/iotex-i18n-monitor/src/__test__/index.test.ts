b5f049f7f974392f43231bdbc98062d1a93a48d3
sxlwar <sxlwar@163.com>
2019-07-12 15:26:54 +0800

    fix: add lint test for i18n-monitor

---
+++ packages/iotex-i18n-monitor/src/__test__/index.test.ts
@@ -0,0 +1,11 @@
+import test from "ava";
+// import { i18nMonitor } from "../index";
+
+test("i18n monitor run", t => {
+  // i18nMonitor({
+  //   source: "../translations/",
+  //   monitorLanguages: ["en", "zh-CN"],
+  //   output: "../translations/update/"
+  // });
+  t.pass();
+});
